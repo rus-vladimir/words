8ec4144ecd34a50ee580cf57cee3b3730e75c65e
Vladimir Rusanov <vrus9787@gmail.com>
2025-03-08 20:35:40 +0200

    Revert fe url

--- wordru_web/src/api/config/apiHooks.tsx
+++ wordru_web/src/api/config/apiHooks.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const wordsAxios = axios.create({
-  //baseURL: "http://petrol.trade:8081/",
-  baseURL: "http://localhost:8081/",
+  baseURL: "http://petrol.trade:8081/",
+  //baseURL: "http://localhost:8081/",
 });
